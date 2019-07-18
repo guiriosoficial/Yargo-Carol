04f54e9b784e9b857299dc4aac2017b65dd991c8
Guilherme Rios <guilherme.rc.98@gmail.com>
2019-07-18 16:20:23 -0300

    Cria parallax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,36 @@
-console.log('\'Allo \'Allo!');
-
+$(function(){
+	$(window).scroll(function(){
+		var scrolltop = $(this).scrollTop();
+		
+		// Passa por cada elemento com a classe .parallax
+		$('.image').each(function(){
+			var obj = $(this);
+			
+		// Garante que apenas trabalhemos no elemento que está visível na tela
+		if ( scrolltop >= obj.position().top - obj.height() 
+		&& scrolltop <= obj.position().top + obj.height()) {
+			
+			// O atributo data-divisor vai definir a velocidade do efeito
+			var divisor = 4;
+			
+			// Corrige a diferença do primeiro elemento
+			if ( obj.is(':first-child') ) {
+				var bg_pos = ( scrolltop - obj.position().top ) / divisor;
+			} else {
+				var bg_pos = ( scrolltop - obj.position().top + ( obj.height() - 100 ) ) / divisor;
+			}
+			
+			// Modifica a posição do bg
+			obj.css({
+				'background-position' : '50% -' + bg_pos + 'px'
+			});
+			
+			// Animação do primeiro texto
+			obj.children('.text').css({
+				'bottom' : ( scrolltop - obj.position().top + 100 ) + 'px'
+			});
+			
+		}
+		});
+	});
+});
