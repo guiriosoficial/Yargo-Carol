d18e8e76d03feee1562f398567a2e0ad61817da2
riwersxd <guilherme.rc.98@gmail.com>
2019-07-19 01:01:32 -0300

    Build Corrigida

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,36 +1,10 @@
-$(function(){
-	$(window).scroll(function(){
-		var scrolltop = $(this).scrollTop();
-		
-		// Passa por cada elemento com a classe .parallax
-		$('.image').each(function(){
-			var obj = $(this);
-			
-		// Garante que apenas trabalhemos no elemento que está visível na tela
-		if ( scrolltop >= obj.position().top - obj.height() 
-		&& scrolltop <= obj.position().top + obj.height()) {
-			
-			// O atributo data-divisor vai definir a velocidade do efeito
-			var divisor = 4;
-			
-			// Corrige a diferença do primeiro elemento
-			if ( obj.is(':first-child') ) {
-				var bg_pos = ( scrolltop - obj.position().top ) / divisor;
-			} else {
-				var bg_pos = ( scrolltop - obj.position().top + ( obj.height() - 100 ) ) / divisor;
-			}
-			
-			// Modifica a posição do bg
-			obj.css({
-				'background-position' : '50% -' + bg_pos + 'px'
-			});
-			
-			// Animação do primeiro texto
-			obj.children('.text').css({
-				'bottom' : ( scrolltop - obj.position().top + 100 ) + 'px'
-			});
-			
-		}
+$(document).ready(function(){
+	$('.image').each(function(){
+	  var $obj = $(this);
+	  $(window).scroll(function() {
+		var yPos = - ( $(window).scrollTop() / 3 ); 
+		var bgpos = '50% '+ yPos + 'px';
+		$obj.css('background-position', bgpos );
 		});
-	});
+	}); 
 });
