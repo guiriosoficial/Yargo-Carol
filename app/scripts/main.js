284cbfdb07f8a547580296a6c0b1d7fdd9ab5db7
riwersxd <guilherme.rc.98@gmail.com>
2019-10-17 02:49:35 -0300

    Cria validação JS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -40,9 +40,9 @@ $(document).ready(function () {
     ev.preventDefault()
     $('#gifts-modal').css({display: 'flex'})
   })
-  $('[data-modal="confirmation-modal"]').submit(function (ev) {
-    $('#confirmation-modal').css({display: 'flex'})
-  })
+  // $('[data-modal="confirmation-modal"]').submit(function (ev) {
+  //   $('#confirmation-modal').css({display: 'flex'})
+  // })
   $('.close, .modal-container').click(function (ev) {
     if (ev.target.className === 'close' || ev.target.className === 'modal-container') {
       ev.preventDefault()
@@ -64,7 +64,45 @@ $(document).ready(function () {
   $('#form-phone').mask(phoneMask, options);
 
 
-  $('#form-submit').click(function (ev) {
-    console.log(ev)
+  // The Validators
+  var name_format = /^[A-ZÀ-Ÿ][A-zÀ-ÿ']+\s([A-zÀ-ÿ']\s?)*[A-ZÀ-Ÿ][A-zÀ-ÿ']+$/
+  var email_format = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gm
+  var phone_format = /^1\d\d(\d\d)?$|^0800 ?\d{3} ?\d{4}$|^(\(0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d\) ?|0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d[ .-]?)?([9,8]|[9,8][ .-])?[2-9]\d{3}[ .-]?\d{4}$/gm
+
+  function name_valid (name) {
+      return name_format.test(name.val())
+  }
+  function email_valid (email) {
+      return email_format.test(email.val())
+  }
+  function phone_valid (phone) {
+      return phone_format.test(phone.val())
+  }
+
+  var name_input = $('#form-name')
+  var email_input = $('#form-email')
+  var phone_input = $('#form-phone')
+
+  $('#form-name').keyup(function (ev) {  
+    !name_valid(name_input) ? name_input.addClass('isInvalid') : name_input.removeClass('isInvalid, nameInvalid')
   })
+  $('#form-email').keyup(function (ev) {
+    !email_valid(email_input) ? email_input.addClass('isInvalid') : email_input.removeClass('isInvalid, emailInvalid')
+  })
+  $('#form-phone').keyup(function (ev) {
+    !phone_valid(phone_input) ? phone_input.addClass('isInvalid') : phone_input.removeClass('isInvalid, phoneInvalid')
+  })
+  
+  $('#confirmation-form').submit(function (ev) {
+    if (name_valid && email_valid && phone_valid) {
+      
+    } else {
+      ev.preventDefault()
+      !name_valid(name_input) ? name_input.addClass('isInvalid, nameInvalid') : name_input.removeClass('isInvalid, nameInvalid')
+      !email_valid(email_input) ? email_input.addClass('isInvalid, emailInvalid') : email_input.removeClass('isInvalid, emailInvalid')
+      !phone_valid(phone_input) ? phone_input.addClass('isInvalid, phoneInvalid') : phone_input.removeClass('isInvalid, phoneInvalid')
+    }
+  })
+
+
 })
